02f687d6637d9ab7a1770a2ed10641be52c746e7
minte9 <catalinprescure@gmail.com>
2022-02-08 14:25:59 +0200

    php-pages update

--- main/symfony/flex/src/Controller/DefaultController.php
+++ main/symfony/flex/src/Controller/DefaultController.php
@@ -15,11 +15,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class DefaultController extends AbstractController 
 {
     /**
+     * @Route("/")
+     */
+    public function index() 
+    {
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
      * @Route("/hello/{name}")
      */
     public function hello($name)
     {
-        return $this->render('default/index.html.twig', [
+        return $this->render('default/hello.html.twig', [
             'name' => $name,
         ]);
     }
