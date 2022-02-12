7ca52765b015575b953b899da0e29057a128a2c9
minte9 <catalinprescure@gmail.com>
2022-02-12 21:29:11 +0200

    php-pages update

--- main/symfony/controllers/src/Controller/LogController.php
+++ main/symfony/controllers/src/Controller/LogController.php
@@ -16,7 +16,10 @@
  * 
  * http://localhost:8000/log/123
  *      # var/log/dev.log
- *      # [2022-02-12] app.INFO: We are loggin id 123       
+ *      # [2022-02-12] app.INFO: We are loggin id 123   
+ * 
+ * http://localhost:8000/setsession     # Session foo set
+ * http://localhost:8000/getsession     # Session foo get: bar
  */
 namespace App\Controller;
 
@@ -24,6 +27,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class LogController extends AbstractController
 {
@@ -35,5 +39,25 @@ class LogController extends AbstractController
         $logger->info("We are loggin id $id");
 
         return new Response('logging');
+    }
+
+    /**
+     * @Route("/setsession")
+     */
+    public function setsession(SessionInterface $session)
+    {
+        $session->set('foo', 'bar');
+
+        return new Response("Session foo set");
+    }
+
+    /**
+     * @Route("/getsession")
+     */
+    public function getsession(SessionInterface $session) 
+    {
+        $foo = $session->get('foo', 'bar');
+
+        return new Response("Session foo get: $foo");
     }
 }
