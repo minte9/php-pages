56e4e629c907d9cd72c44b874b3687faf06472e1
minte9 <catalinprescure@gmail.com>
2022-02-13 13:09:00 +0200

    php-pages update

---
+++ main/basics/constants/constants.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Contanstans
+ * 
+ * Constants can be defined using the const keyword ...
+ * or by using the define()
+ * 
+ * Can't be changed once set.
+ * Can be accessed for any scope within a script.
+ * Constants may only evaluate to scalar values or arrays.
+ */
+
+error_reporting(E_ALL);
+
+define('A', '1');
+define('A', '2'); // Warning: Constant CCC already defined 
+
+const B = 2;
+const C = array(3);
+
+{ 
+    echo A;     // 1
+    echo B;     // 2
+    echo C[0];  // 3
+}
