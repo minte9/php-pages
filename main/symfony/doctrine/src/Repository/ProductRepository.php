a46149539796c6e529f42ae2774b64d789b5cbb3
minte9 <catalinprescure@gmail.com>
2022-02-13 17:48:53 +0200

    php-pages update

--- main/symfony/doctrine/src/Repository/ProductRepository.php
+++ main/symfony/doctrine/src/Repository/ProductRepository.php
@@ -21,7 +21,8 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Product|null find($id, $lockMode = null, $lockVersion = null)
  * @method Product|null findOneBy(array $criteria, array $orderBy = null)
  * @method Product[]    findAll()
- * @method Product[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Product[]    findBy(array $criteria, 
+ *      array $orderBy = null, $limit = null, $offset = null)
  */
 class ProductRepository extends ServiceEntityRepository
 {
