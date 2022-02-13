df56a85aae3db2d3124978b77cb3e843eaff280a
minte9 <catalinprescure@gmail.com>
2022-02-13 12:40:18 +0200

    php-pages update

---
+++ main/basics/herodoc.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Herodoc strings
+ * 
+ * It is used for formating on multiple lines. 
+ * You can use of quotes without escaping.
+ * 
+ * The closing identifier must contain no characters, except a semicolon (;)
+ * The first character after the closing identifier must be a newline. 
+ */
+
+$who = "John";
+$output = <<<TEST
+    She said "This is $who's test"
+    on multiple rows
+TEST;
+
+echo nl2br($output);
+/* 
+    She said "This is John's test"
+    on multiple rows
+*/
