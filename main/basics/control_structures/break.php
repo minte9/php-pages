d1d28bde3ffc3768ef82e71cf8bcd08a579fa65d
minte9 <catalinprescure@gmail.com>
2022-02-13 19:04:51 +0200

    php-pages update

--- main/basics/control_structures/break.php
+++ main/basics/control_structures/break.php
@@ -4,8 +4,7 @@ for ($i=0; $i<=10; $i++) {
 
     for ($j=0; $j<=10; $j++) {
 
-        if ($j == 5) break 2; 
-            // Exist this loop and the next one
+        if ($j == 5) break 2; // Exist this loop and the next one
         
         echo $i; // 00000
     }
