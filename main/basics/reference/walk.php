8c47e1252c00cbe06b7349764685db6edbaa75b8
minte9 <catalinprescure@gmail.com>
2022-02-14 22:34:06 +0200

    php-pages update

--- main/basics/reference/walk.php
+++ main/basics/reference/walk.php
@@ -5,17 +5,14 @@
  */
 
 $A = [1.5, 2.6, 3.7];
-array_walk($A, fn($v) => $v = floor($v));
+array_walk($A, fn($v) => $v = floor($v)); // 1.5, 2.6, 3.7
 print_r($A); 
-    // 1.5, 2.6, 3.7
     
 $A = [1.5, 2.6, 3.7];
-array_walk($A, fn(&$v) => $v = floor($v));
+array_walk($A, fn(&$v) => $v = floor($v)); // 1, 2, 3
 print_r($A); 
-    // 1, 2, 3
-
+    
 // map
 $A = [1.5, 2.6, 3.7];
-$A = array_map('floor', $A);
+$A = array_map('floor', $A); // 1, 2, 3
 print_r($A); 
-    // 1, 2, 3
