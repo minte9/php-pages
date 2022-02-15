a4556129685370c8f7ab2619fc818d0c6df9d260
minte9 <catalinprescure@gmail.com>
2022-02-15 21:18:41 +0200

    php-pages update

--- main/basics/integers/binary.php
+++ main/basics/integers/binary.php
@@ -1,8 +1,8 @@
 <?php
 
-echo 0b110100100; // 420 - decimal
-echo bindec(110100100); // 420 - decimal
-echo decoct(0b110100100); // 644 - octal
+echo 0b110100100;           // 420 - decimal
+echo bindec(110100100);     // 420 - decimal
+echo decoct(0b110100100);   // 644 - octal
 
 echo 0644;
     // 6*8^2 + 4*8^1 + 4*8^0
