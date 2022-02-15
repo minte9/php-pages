7418ca8491213fb0fb66be739751256f1a580de4
minte9 <catalinprescure@gmail.com>
2022-02-15 21:23:56 +0200

    php-pages update

--- main/basics/integers/float.php
+++ main/basics/integers/float.php
@@ -4,5 +4,5 @@
  * The result of this simple expression is stored internally as 7.999999
  */
 
-echo (0.1 + 0.7) * 10;          // 8
-echo (int) ((0.1 + 0.7) * 10);  // 7
+echo (0.1 + 0.7) * 10; // 8
+echo (int) ((0.1 + 0.7) * 10); // 7
