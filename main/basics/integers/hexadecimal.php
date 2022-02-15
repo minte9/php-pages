2544b1c93d37ca85e4fcb5a24ac775c040f3a0c1
minte9 <catalinprescure@gmail.com>
2022-02-15 20:36:13 +0200

    php-pages update

---
+++ main/basics/integers/hexadecimal.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Hex numbers are often used to represent colours.
+ * Orange is represented as #FFA500 (255 red, 165 green, 0 blue).
+ */
+
+echo 0x121;     // 1*16^2 + 2*16^1 + 1*16^0 = 256 + 32 + 1 = 289
+echo 0xFF;      // 15*16^1 + 15*16^0 = 240 + 15 = 255
