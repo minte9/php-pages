b1a1b3b8acfa2ed60e1a12e2f178ee0109b0cc86
minte9 <catalinprescure@gmail.com>
2022-02-15 21:22:32 +0200

    php-pages update

--- main/basics/integers/float.php
+++ main/basics/integers/float.php
@@ -1,6 +1,8 @@
 <?php
-
-// The result of this simple expression is stored internally as 7.999999
+/**
+ * Float data type is not always capable of representing numbers.
+ * The result of this simple expression is stored internally as 7.999999
+ */
 
 echo (0.1 + 0.7) * 10;          // 8
 echo (int) ((0.1 + 0.7) * 10);  // 7
