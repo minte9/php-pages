0217d7271d9c02db347ddc5b5a1aef4dd3ef18a7
minte9 <catalinprescure@gmail.com>
2022-02-18 18:12:18 +0200

    php-pages update

--- main/variables/definition/case.php
+++ main/variables/definition/case.php
@@ -1,9 +1,7 @@
 <?php
 /**
  * In PHP Variables are one of only two identifier types ...
- * that are case-sensitive.
- * 
- * The other are constants.
+ * that are case-sensitive. The other are constants.
  */
 
 error_reporting(E_ALL);
