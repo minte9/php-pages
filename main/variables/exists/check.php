0cd51c0edce85b13cd7ee77adca086b23934c08c
minte9 <catalinprescure@gmail.com>
2022-02-20 14:57:55 +0200

    php-pages update

--- main/variables/exists/check.php
+++ main/variables/exists/check.php
@@ -10,10 +10,10 @@ $b = 0;
 $c = "";
 $d = null;
 
-echo isset($a) ? 1 : 0; // 1
-echo isset($b) ? 2 : 0; // 2
-echo isset($c) ? 3 : 0; // 3
-echo isset($d) ? 4 : 0; // 0
+echo isset($a)      ? 1 : 0; // 1
+echo isset($b)      ? 2 : 0; // 2
+echo isset($c)      ? 3 : 0; // 3
+echo isset($d)      ? 4 : 0; // 0
 
 echo empty("")      ? 5 : 0; // 5
 echo empty(0)       ? 6 : 0; // 6
