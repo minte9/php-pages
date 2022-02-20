c8f8f884637fec46d5884a96d799cd2bb051ecf3
minte9 <catalinprescure@gmail.com>
2022-02-20 14:57:14 +0200

    php-pages update

--- main/variables/exists/downside.php
+++ main/variables/exists/downside.php
@@ -2,10 +2,14 @@
 
 /**
  * There are downsides of the way PHP handles variables.
- * There is no way to ensure that any one of them will exist at any given point.
+ * There is no way to ensure that any one of them ...
+ * will exist at any given point.
  * 
- * isset() will return TRUE if a variable exists and has a value other than NULL
- * empty() will returns TRUE if var has a empty and zero value ("", 0, NULL)
+ * isset() will return TRUE if a variable ...
+ * exists and has a value other than NULL
+ * 
+ * empty() will returns TRUE if a variable ...
+ * has a empty and zero value ("", 0, NULL)
  */
 
  if ($a) // undefined variable
