f1c3f955efcb0aaa50a117c83822d9700aba65ad
minte9 <catalinprescure@gmail.com>
2022-03-25 17:13:52 +0200

    php-pages update

---
+++ main/framework/routing/public/front.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Front Controller - public/front.php
+ * 
+ * Following the MVC pattern is not the right goal.
+ * The main goal should be Separartions of Concerns SoC.
+ * Route configuration moved to its own file (app.php)
+ * 
+ * Routes names are used for template names.
+ * 500 errors are managed correctly.
+ * 
+ * Based on $routes information (RouteCollection instance),
+ * a UrlMatcher instance can match URL paths.
+ * 
+ * The match() method takes a request path and returns an array of attributes.
+ * The matched route is automaticaly stored in _route attribute.
+ * Attributes are extracted to keep template simple.
+ * 
+ * The request context is not strictly needed, 
+ * but it is used in real-world applications
+ * to enforce method requirements and more.
+ * 
+ * 
+ * composer require symfony/http-foundation
+ * composer require symfony/routing
+ * 
+ * php -S localhost:8000 public/front.php
+ *  
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+$request = Request::createFromGlobals();
+$routes = include __DIR__.'/../src/app.php';
+
+$context = new RequestContext();
+$context->fromRequest($request);
+
+$matcher = new UrlMatcher($routes, $context);
+
+try {
+    extract($matcher->match($request->getPathInfo(), EXTR_SKIP)); 
+        // $_route = hello | bye
+        // $name = Fabien | World
+    
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.phtml', $_route);
+    $response = new Response(ob_get_clean());
+
+} catch(ResourceNotFoundException $e) {
+    $response = new Response('Not found', 404);
+
+} catch (Exception $e) {
+    $response = new Response('An error occured', 500);
+}
+
+$response->send();
---
+++ main/framework/routing/src/app.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * Routes - src/app.php
+ */
+
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+$routes = new RouteCollection();
+
+$routes->add('hello', 
+    new Route('/hello/{name}', ['name' => 'World'])
+);
+$routes->add('bye', 
+    new Route('/bye')
+);
+
+return $routes;
