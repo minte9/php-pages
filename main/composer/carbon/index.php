1ff62ad505d181ae351ca1300532559da1517dcd
minte9 <catalinprescure@gmail.com>
2022-03-28 16:53:40 +0300

    php-pages update

--- main/composer/carbon/index.php
+++ main/composer/carbon/index.php
@@ -1,4 +1,10 @@
-<?php // composer require nesbot/carbon
+<?php 
+
+/**
+ * The Carbon class is inherited from the PHP DateTime class.
+ * 
+ * composer require nesbot/carbon
+ */
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -8,14 +14,28 @@ use Carbon\Carbon;
  * next
  */
 $now = Carbon::now();
+$dt = Carbon::parse($now);
+echo $dt->hour . "\n";
+
 $date = $now->add(1, 'day');
-echo $date->isoFormat('dddd D'); // Tuesday 29
+echo $date->isoFormat('dddd D') . "\n"; 
+    // Tuesday 29
+    
 $date = $now->add(1, 'month');
-echo $date->isoFormat('YYYY-MM-DD'); // 2022-04-29
+echo $date->isoFormat('YYYY-MM-DD') . "\n"; 
+    // 2022-04-29
 
 /**
  * timezone
  */
+$now = Carbon::now();
+$dt = Carbon::parse($now);
+echo $dt->timezone . "\n"; 
+    // Europe/Helsinki
+echo $dt->hour . "\n";
+    // 16
+
 $nowLondon = Carbon::now('Europe/London');
 $dt = Carbon::parse($nowLondon);
-echo $dt->hour; // 13
+echo $dt->hour  . "\n"; 
+    // 14
