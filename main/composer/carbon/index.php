1f4fb7519c61076b5822c3547436407acdcb9b1e
minte9 <catalinprescure@gmail.com>
2022-03-28 15:34:04 +0300

    php-pages update

--- main/composer/carbon/index.php
+++ main/composer/carbon/index.php
@@ -10,14 +10,18 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Carbon\Carbon;
 
-// next
+/**
+ * next
+ */
 $now = Carbon::now();
 $date = $now->add(1, 'day');
 echo $date->isoFormat('dddd D'); // Tuesday 29
 $date = $now->add(1, 'month');
 echo $date->isoFormat('YYYY-MM-DD'); // 2022-04-29
 
-// timezone
+/**
+ * timezone
+ */
 $nowLondon = Carbon::now('Europe/London');
 $dt = Carbon::parse($nowLondon);
 echo $dt->hour; // 13
