87071bc1d9c7d5835ceacfc47aa31f6bffcec42f
minte9 <catalinprescure@gmail.com>
2022-03-28 15:30:38 +0300

    php-pages update

---
+++ main/composer/carbon/index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * index.php
+ * 
+ * composer require nesbot/carbon
+ */
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Carbon\Carbon;
+
+// next
+$now = Carbon::now();
+$date = $now->add(1, 'day');
+echo $date->isoFormat('dddd D'); // Tuesday 29
+$date = $now->add(1, 'month');
+echo $date->isoFormat('YYYY-MM-DD'); // 2022-04-29
+
+// timezone
+$nowLondon = Carbon::now('Europe/London');
+$dt = Carbon::parse($nowLondon);
+echo $dt->hour; // 13
