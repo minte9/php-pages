e537d82756755ff0e18b4291a60a8bf70628ae50
minte9 <catalinprescure@gmail.com>
2022-03-29 12:34:35 +0300

    php-pages update

--- main/composer/faker/index.php
+++ main/composer/faker/index.php
@@ -10,7 +10,7 @@
  * composer require fzaninotto/faker
  */
 
-require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $faker = Faker\Factory::create();
 
---
+++ main/composer/math/index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * composer require markrogoyski/math-php
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+
+use MathPHP\Algebra;
+use MathPHP\Arithmetic;
+use MathPHP\Functions\Map;
+use MathPHP\LinearAlgebra\Matrix;
+use MathPHP\LinearAlgebra\MatrixFactory;
+
+// Greatest common divisor (GCD)
+echo $gcd = Algebra::gcd(8, 12) . "\n"; // 4
+
+// Sum of digits
+echo $digit_sum = Arithmetic::digitSum(99) . "\n"; // 18
+
+// Map functions
+$x = [10, 10, 10, 10];
+$y = [1,   2,  5, 10];
+$products = Map\Multi::multiply($x, $y);
+
+echo implode(", ", $products) . "\n"; // 10, 20, 50, 100
+
+// Matrix
+$matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+$A = MatrixFactory::create($matrix);
+$row = $A->getRow(2);
+
+echo implode(", ", $row) . "\n"; // 7, 8, 9
