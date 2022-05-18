49084f656ed83643486b382a3690564954ba3b7c
minte9 <catalinprescure@gmail.com>
2022-05-18 10:14:30 +0300

    php-pages update

--- main/operators/string/concatenation_equal.php
+++ main/operators/string/concatenation_equal.php
@@ -5,7 +5,7 @@ $b = "b";
 
 echo $a .= $b; // ab
 
-# Using the + operator (like in Java) ...
+# Using the + operator, like in Java ...
 # will result in TypeError (PHP 8+)
 
 echo $a += $b; // Unsupported operant types
