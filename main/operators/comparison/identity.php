2b2efa5d1db26bc74547da3bfe9b35877e00ebb5
minte9 <catalinprescure@gmail.com>
2022-05-18 10:32:08 +0300

    php-pages update

--- main/operators/comparison/identity.php
+++ main/operators/comparison/identity.php
@@ -1,9 +1,11 @@
 <?php
 
 // Identical
+
 $domain = 'xn--google.com';
 echo (stripos($domain, 'xn--') === 0); // true - Correct
 
 // Equal
+
 $domain2 = 'google.com';
 echo (stripos($domain2, 'xn--') == 0); // true - Incorrect
