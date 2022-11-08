04d2fe4c751ffde0cb9deed4d74192788eb3dcf9
minte9 <catalinprescure@gmail.com>
2022-11-08 18:47:44 +0200

    php-pages update

--- main/function/anonymous/callbacks.php
+++ main/function/anonymous/callbacks.php
@@ -1,13 +1,14 @@
 <?php
 
 $arr = array(0, 3, 10);
-$data = array_filter ($arr, function($x) { // isEven
+
+$data1 = array_filter ($arr, function($x) { // isEven
     return $x % 2 == 0;
 });  
-print_r($data); // [0, 100]
 
-$arr = array(3, 10);
-$data = array_map(function ($x) { // square
+$data2 = array_map(function ($x) { // square
     return $x*$x; 
 }, $arr); 
-print_r($data); // [9, 100]
+
+print_r($data1); // [0, 100]
+print_r($data2); // [0, 9, 100]
