8f74bf3f6e5f5d473e312446d8180a571dc340d4
minte9 <catalinprescure@gmail.com>
2022-11-10 19:12:57 +0200

    php-pages update

--- main/array/create/merge.php
+++ main/array/create/merge.php
@@ -12,21 +12,19 @@
 $A = array_merge((array) 'foo', [1, 2]);  
 print_r($A); // foo, 1, 2 
 
-# 
 $B = array_merge(
-    ['color'=> 'red', 1], // string keys - overwritten
+    ['color'=> 'red', 1], // string keys
     ['a', 'color'=>'green']
 ); 
 print_r($B); // [color] => green [0] => 1 [1] => a
 
-#
 $B = array_merge(
-    [1, 2], // numeric keys - not overwritten
+    [1, 2], // numeric keys
     [3, 4]
 ); 
 print_r($B); // [0] => 1 [1] => 2 [2] => 3 [3] => 4
 
-# 
-$C = ['color'=> 'red', 1] + // array union operator (+)
-     ['color'=>'green', 2, 3];
+$C = ['color'=> 'red', 1] +
+     ['color'=>'green', 2, 3]
+;
 print_r($C); // [color] => red [0] => 1 [1] => 3
