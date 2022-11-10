25434f0849003e7c40bdb95657f4f6bb0f2cfdc6
minte9 <catalinprescure@gmail.com>
2022-11-10 19:01:41 +0200

    php-pages update

--- main/array/create/merge.php
+++ main/array/create/merge.php
@@ -1,7 +1,34 @@
 <?php
 
 /**
+ * Merge one or more arrays
  * 
+ * If arrays have the same string keys ...
+ * the later value for that key will be overwriten
+ * 
+ * If don't want to ovewrite use the + array union operator
  */
 
- 
+$A = array_merge((array) 'foo', [1, 2]);  
+print_r($A); // foo, 1, 2 
+
+#
+
+$B = array_merge(
+    ['color'=> 'red', 1], // string keys overwritten
+    ['a', 'color'=>'green']
+); 
+print_r($B); // [color] => green [0] => 1 [1] => a
+
+$B = array_merge(
+    [1, 2], // numeric keys not overwritten
+    [3, 4]
+); 
+print_r($B); // [0] => 1 [1] => 2 [2] => 3 [3] => 4
+
+#
+
+$A = ['color'=> 'red', 1];
+$B = ['color'=>'green', 2, 3];
+$C = $A + $B; 
+print_r($C); // [color] => red [0] => 1 [1] => 3
