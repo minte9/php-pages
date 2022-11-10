4cfa68fc66e860c1370bf634d619d33c6bcf0fb3
minte9 <catalinprescure@gmail.com>
2022-11-10 11:13:32 +0200

    php-pages update

---
+++ main/array/array_basics/list.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * List constructor
+ * Very usefull for string explode
+ */
+
+list($surname, $name, $age) = ["John", "Smith", "34", "07288333"];
+
+echo "$surname $name is $age years old"; 
+    // Ouput: John Smith is 34 years old
+
+$date = '25/05/2012';
+list($day, $month, $year) = explode('/', $date);
+echo $month; // 05
