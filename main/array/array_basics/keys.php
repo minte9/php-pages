109605c12bb0e653e2ae19852310e220d00bb2ed
minte9 <catalinprescure@gmail.com>
2022-11-10 11:22:11 +0200

    php-pages update

--- main/array/array_basics/keys.php
+++ main/array/array_basics/keys.php
@@ -16,11 +16,11 @@ $b[] = 44; // This will have a key of 5
 echo $b[5]; // 44
 
 $c = array (
-    'A' => 1,
-    'a' => 2,
-    '1' => 3,
-    1 => 4,
-    '01' => 5, // not equal with 1
+    'A'  => 1,
+    'a'  => 2,
+    '1'  => 3, // equal with 1
+     1   => 4,
+    '01' => 5, // NOT equal with 1
 );
 
-var_dump($c); // [A=>1, a=>2, 1=>3, 01=>5]
+var_dump($c); // [A=>1, a=>2, 1=>4, 01=>5]
