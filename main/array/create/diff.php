3ca988f81bf19ab742d60e7a293d9f6e2f445f6c
minte9 <catalinprescure@gmail.com>
2022-11-10 18:49:10 +0200

    php-pages update

--- main/array/create/diff.php
+++ main/array/create/diff.php
@@ -1,7 +1,18 @@
 <?php
 
 /**
+ * Diff returns ...
+ * all values in an array not appearing in another
  * 
+ * Intersect keeps ...
+ * all the values of each element
  */
 
- 
+$A = [1, 2, 3];
+$B = [1, 3, 4];
+
+$C = array_diff($A, $B);
+print_r($C); // [1] => 2
+
+$C = array_intersect($A, $B);
+print_r($C); // [0] => 1 [2] => 3
