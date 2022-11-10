f14c2426e99bc5599c4ec5a8d68ada366d8c72fe
minte9 <catalinprescure@gmail.com>
2022-11-10 17:32:55 +0200

    php-pages update

---
+++ main/array/operations/map_walk.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Array_map() with function Name ...
+ * or arrow function
+ */
+
+function cube($n) 
+{
+    return pow($n, 3);
+}
+$res1 = array_map('cube', [1, 2, 3]); 
+print_r($res1); // [1, 8, 27] 
+
+# 
+
+$res2 = array_map(
+    fn($x) => pow($x, 3), [1, 2, 3]
+);
+print_r($res2); // [ 1, 8, 27 ]
+
+
+/** 
+ * Array_walk() needs reference
+ */
+
+$arr = array(1,2,3,4);
+function setDouble($value) 
+{ 
+    $value * 2;
+}
+array_walk($arr, 'setDouble');
+print_r($arr); // 1 2 3 4
+
+
+$arr = array(1,2,3,4);
+function setDouble2(&$value) // reference - Look Here
+{ 
+    $value *= 2;
+}
+array_walk($arr, 'setDouble'); 
+print_r($arr); // [2, 4, 6, 8]
