9326bb7816f455108b64db251221274e0175b018
minte9 <catalinprescure@gmail.com>
2022-11-10 18:32:59 +0200

    php-pages update

--- main/array/operations/map_walk.php
+++ main/array/operations/map_walk.php
@@ -8,27 +8,32 @@
 
 # Map
 
+$A = [1, 2, 3];
+$B = [1, 2, 3];
+
 function cube($n) 
 {
     return pow($n, 3);
 }
-$A = array_map('cube', [1, 2, 3]); 
-$B = array_map(
-    fn($x) => pow($x, 3), [1, 2, 3]
-);
+
+$A = array_map('cube', $A); 
+$B = array_map(fn($x) => pow($x, 3), $B);
+
 print_r($A); // 1, 8, 27
 print_r($B); // 1, 8, 27
 
 # Walk
 
-$A = [1, 2, 3];
-$B = [1, 2, 3];
+$C = [1, 2, 3];
+$D = [1, 2, 3];
+
 function setDouble($value) // incorrect (no reference)
 { 
     $value * 2;
 }
-array_walk($A, 'setDouble');
-print_r($A); // 1 2 3
 
-array_walk($B, fn(&$x) => $x *= 2); // reference - Look Here
-print_r($B); // [2, 4, 6]
+array_walk($C, 'setDouble');
+print_r($C); // 1 2 3
+
+array_walk($D, fn(&$x) => $x *= 2); // with reference - Look Here
+print_r($D); // 2, 4, 6
