1f06ab64a039174589fce3eef80e408149859bed
minte9 <catalinprescure@gmail.com>
2022-11-18 17:26:21 +0200

    php-pages update

--- main/array/sort/filter.php
+++ main/array/sort/filter.php
@@ -2,9 +2,12 @@
 
 /**
  * array_filter()
+ * 
+ * Without a callback returns non empty values
  */
 
 $A = [1, 2, 3, 4, 5, 6];
+$B = [1, false, null, 'b'];
 
 function odd($var) : int
 {
@@ -18,3 +21,4 @@ function isEven($var) : bool
 
 print_r(array_filter($A, 'odd'));    // [1, 3. 5]
 print_r(array_filter($A, 'isEven')); // [2, 4, 6]
+print_r(array_filter($B));           // [0 => 1, 3 => b]
