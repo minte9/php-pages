7767477ae069bf35ea67440ca30321c0d7a99ded
minte9 <catalinprescure@gmail.com>
2022-11-18 14:19:54 +0200

    php-pages update

--- main/function/anonymous/callbacks.php
+++ main/function/anonymous/callbacks.php
@@ -5,8 +5,14 @@
  */
 
 $A = array(0, 3, 10);
-$B = array_filter( $A, fn($x) => $x % 2 == 0 );  // is even
-$C = array_map( fn($x) => $x*$x, $A) ; // square
+
+$B = array_filter(
+    $A, fn($x) => $x % 2 == 0 // is even
+);  
+
+$C = array_map(
+    fn($x) => $x*$x, $A) // square
+; 
 
 print_r($B); // 0, 100
 print_r($C); // 0, 9, 100
