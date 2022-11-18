3b8eab4a5b31861ebac3a646985f3b390396519a
minte9 <catalinprescure@gmail.com>
2022-11-18 15:51:26 +0200

    php-pages update

--- main/array/modify/slice.php
+++ main/array/modify/slice.php
@@ -11,6 +11,8 @@ $A = [1, 2, 3, 4, 5];
 print_r(array_slice($A, 2)); // 3, 4, 5
 print_r(array_slice($A, 0, 2)); // 1, 2
 print_r(array_slice($A, -2, 1)); // [0] => 4
+print_r(array_slice($A, -2, 1, true)); // [3] => 4 (preserve keys)
 
-// preserve keys
-print_r(array_slice($A, -2, 1, true)); // [3] => 4
+$B = array(1, 2, 3, 4, 5);
+
+print_r(array_splice($B, 2)); // [ 1, 2 ]
