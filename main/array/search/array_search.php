2aa94549f19d5e070e263c3cedff24862a7cd8bb
minte9 <catalinprescure@gmail.com>
2022-11-18 15:04:55 +0200

    php-pages update

--- main/array/search/array_search.php
+++ main/array/search/array_search.php
@@ -6,10 +6,7 @@
  */
 
 $A = [1,2,3,4];
-echo array_search(4, $A); // 3
 
-// wrong operator (==)
+echo (array_search(4, $A)); // 3
 echo (array_search(5, $A) == 0) == true; // 1 - Incorrect
-
-// correct operator (===)
 echo (array_search(5, $A) === 0) === false; // 1 - Correct
