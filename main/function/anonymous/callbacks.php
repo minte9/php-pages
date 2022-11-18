20bd7dc5906000ec074653c048576657995537a4
minte9 <catalinprescure@gmail.com>
2022-11-18 14:16:17 +0200

    php-pages update

--- main/function/anonymous/callbacks.php
+++ main/function/anonymous/callbacks.php
@@ -6,13 +6,9 @@
 
 $A = array(0, 3, 10);
 
-$B = array_filter (
-    $A, fn($x) => $x % 2 == 0 // isEven  
-); 
+$B = array_filter( $A, fn($x) => $x % 2 == 0 ); // isEven  
 
-$C = array_map(
-    fn($x) => $x*$x, $A // square 
-);
+$C = array_map( fn($x) => $x*$x, $A) ; // square 
 
 print_r($B); // [0, 100]
 print_r($C); // [0, 9, 100]
