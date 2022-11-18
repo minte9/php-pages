305bd69b68d836d42274c910ecc999c6583a032f
minte9 <catalinprescure@gmail.com>
2022-11-18 14:57:57 +0200

    php-pages update

--- main/array/search/value_exists.php
+++ main/array/search/value_exists.php
@@ -7,6 +7,6 @@
 $A = ['a'=>1, 'b'=>2, 'c'=>3];
 $B = ['a'=>NULL, 'b'=>2]; 
 
-echo (in_array(1, $A) === true); # 1
-echo (isset($B['a']) === false); # 1
-echo (array_key_exists('a', $B) === true); # 1
+echo (in_array(1, $A) === true); // 1
+echo (isset($B['a']) === false); // 1
+echo (array_key_exists('a', $B) === true); // 1
