cf11e27f28e0d966f4a3634be219cace92126ba2
minte9 <catalinprescure@gmail.com>
2022-11-18 17:20:50 +0200

    php-pages update

--- main/array/sort/filter.php
+++ main/array/sort/filter.php
@@ -1,5 +1,20 @@
-<?php
+<?php declare(strict_types=1);
 
 /**
- * 
+ * array_filter()
  */
+
+$A = [1, 2, 3, 4, 5, 6];
+
+function isOdd($var) : int
+{
+    return ($var & 1); // bits set in both
+}
+
+function isEven($var) : bool
+{
+    return ! ($var & 1); // bit 1 not set
+}
+
+print_r(array_filter($A, 'isOdd')); //  [1, 3. 5]
+print_r(array_filter($A, 'isEven'));// [2, 4, 6]
