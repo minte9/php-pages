e5a53925fec9fad7f456de27be0d36be4209b628
minte9 <catalinprescure@gmail.com>
2022-11-18 15:42:19 +0200

    php-pages update

--- main/array/modify/padding.php
+++ main/array/modify/padding.php
@@ -1,5 +1,14 @@
 <?php
 
 /**
+ * array_pad()
  * 
+ * Pad an array to the specified length
  */
+
+$A = [1,2,3];
+$B = array_pad($A, 6, 0); 
+
+print_r($B); // 1, 2, 3, 0, 0, 0
+
+echo str_pad(111, 6, 0); // 111000
