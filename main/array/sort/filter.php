5df04246fecd3a951cee6fc78414cb975d1b63d1
minte9 <catalinprescure@gmail.com>
2022-11-18 17:23:03 +0200

    php-pages update

--- main/array/sort/filter.php
+++ main/array/sort/filter.php
@@ -6,7 +6,7 @@
 
 $A = [1, 2, 3, 4, 5, 6];
 
-function isOdd($var) : int
+function odd($var) : int
 {
     return ($var & 1); // bits set in both
 }
@@ -16,5 +16,5 @@ function isEven($var) : bool
     return ! ($var & 1); // bit 1 not set
 }
 
-print_r(array_filter($A, 'isOdd')); //  [1, 3. 5]
+print_r(array_filter($A, 'odd')); //  [1, 3. 5]
 print_r(array_filter($A, 'isEven'));// [2, 4, 6]
