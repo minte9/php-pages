1b75deab48e84ffad60b87bb57575feb077b168b
minte9 <catalinprescure@gmail.com>
2022-11-18 17:30:42 +0200

    php-pages update

--- main/array/sort/multisort.php
+++ main/array/sort/multisort.php
@@ -1,5 +1,28 @@
 <?php
 
 /**
+ * array_multisort()
  * 
+ * Sorting destroys all the keys and rearange elements
  */
+
+$A = [3,2,1];
+$B = [4,8,0];
+
+array_multisort($A, $B);
+
+print_r($A); // 1, 2, 3
+print_r($B); // 0, 8, 4
+ 
+$C = [
+    ["10", 11, 100, 100, "a"],
+    [1, 2, "2", 3, 1],
+];
+
+array_multisort(
+    $C[0], SORT_ASC, SORT_STRING,
+    $C[1], SORT_DESC, SORT_NUMERIC);
+
+print_r($C);
+    // ["10", 100, 100, 11, "a"]
+    // [ 1, 3, "2", 2, 1 ]
