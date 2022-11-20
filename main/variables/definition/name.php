e684e590a646948bfedccafd49eafa28ea78a003
minte9 <catalinprescure@gmail.com>
2022-11-20 21:28:36 +0200

    php-pages update

--- main/variables/definition/name.php
+++ main/variables/definition/name.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * Variables must be named using only letters, numbers and underscore.
+ * Variables must be named using only ...
+ * letters, numbers and underscore.
  *      $name/ - invalid name
  * 
- * Variables must start with a letter or an underscore.
+ * Variables must start with a letter or an underscore
  *      $1name - invalid start
  */
 
