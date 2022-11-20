ccade535fb43e1fba9293b1ec6f90505b62fa981
minte9 <catalinprescure@gmail.com>
2022-11-20 11:22:14 +0200

    php-pages update

--- main/array/create/merge.php
+++ main/array/create/merge.php
@@ -10,14 +10,8 @@
  */
 
 $A = array_merge((array) 'foo', [1, 2]);  
-$B = array_merge(
-    ['color'=> 'red', 1], // string keys
-    ['a', 'color'=>'green']
-);
-$C = ['a' => 'A', 1]
-        +
-     ['a' => 'B', 2, 3]
-;
+$B = array_merge(['color'=> 'red', 1], ['a', 'color'=>'green']);
+$C = ['a' => 'A', 1] + ['a' => 'B', 2, 3];
 
 echo 'A: ' . print_r($A, true); // foo, 1, 2 
 echo 'B: ' . print_r($B, true); // green, 1, a
