caf04b2237ab0f0a3ffcbaac4408a1e6acdd3df0
minte9 <catalinprescure@gmail.com>
2022-11-20 12:16:28 +0200

    php-pages update

--- main/array/search/value_exists.php
+++ main/array/search/value_exists.php
@@ -9,4 +9,4 @@ $B = ['a'=>1, 'b'=>NULL];
 
 var_dump( in_array(1, $A) ); // true
 var_dump( isset($B['b']) ); // false
-var_dump( isset(['a'=>1, 'b'=>NULL]['b']) ); // false
+var_dump( isset(['a'=>1]['b']) ); // false
