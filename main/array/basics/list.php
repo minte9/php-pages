5dabdd9efba28cdd8dd258e24e634ec86f23bf42
minte9 <catalinprescure@gmail.com>
2022-11-20 11:27:16 +0200

    php-pages update

--- main/array/basics/list.php
+++ main/array/basics/list.php
@@ -4,16 +4,21 @@
  * list() 
  * 
  * Not really a function, but a language construct
+ * List doesn't work with strings, use explode()
  * 
- * List doesn't work with strings
- * Often used with string explode()
+ * Extract assign multiple variables ...
+ * from key/value array
  */
 
 $USER = ["John", "34", "07288333"];
 $date = '25/05/2012';
+$DATA = ['b'=>2, 'c'=>3];
 
 list($name, $age) = $USER;
 list($day, $month, $year) = explode('/', $date);
 
 print_r("$name is $age years old"); // John is 34 years old
 print_r($month); // 05
+
+extract($DATA);
+var_dump( $b == 2); // true
