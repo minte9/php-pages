6e1a19a95372ff2996e4f1c42e57bb294bb484af
minte9 <catalinprescure@gmail.com>
2022-11-20 13:30:07 +0200

    php-pages update

--- main/basics/reference/array.php
+++ main/basics/reference/array.php
@@ -1,13 +1,16 @@
 <?php
+
 /**
- * Reference with foreach changes the array.
- * Use it with care.
+ * &$value
+ * 
+ * Reference with foreach changes the array
+ * Use it with care
  */
 
-$arr = array('1', '2', '3', '4');
+$A = [1, 2, 3, 4];
 
-foreach ($arr as &$val) {
+foreach ($A as &$val) { // Look Here
     $val *= 2;
 }
 
-print_r($arr); // [2, 4, 6, 8]
+var_dump($A === [2, 4, 6, 8]); // true
