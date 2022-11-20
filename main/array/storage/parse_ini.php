3270fa4cf02a653f5d872fa2abe844651d2de886
minte9 <catalinprescure@gmail.com>
2022-11-20 12:42:21 +0200

    php-pages update

--- main/array/storage/parse_ini.php
+++ main/array/storage/parse_ini.php
@@ -1,6 +1,16 @@
 <?php
 
 /**
+ * config.ini
  * 
+ * url_base = "api.com/RestApi/v1/"
+ * ssl_verifyhost = 2
+ * ssl_verifypeer = true
+ * auth_type = "token"
+ * auth_method = "Authentication/RequestToken"
  */
 
+$conf = parse_ini_file(__DIR__ . '/inc/config.ini');
+
+var_dump($conf['ssl_verifyhost'] == 2);  // true
+var_dump($conf['auth_type'] == 'token'); // true
