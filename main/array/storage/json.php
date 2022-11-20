752f24b2cd396fd8a74fa6f940208c401b15cda3
minte9 <catalinprescure@gmail.com>
2022-11-20 12:35:39 +0200

    php-pages update

--- main/array/storage/json.php
+++ main/array/storage/json.php
@@ -1,6 +1,12 @@
 <?php
 
 /**
- * 
+ * Encode anaray, json representation
  */
 
+$A = ['a' => 1, 'b' => 2, 'c' => 3];
+$json_encoded = json_encode($A);
+$json_decoded = json_decode($json_encoded);
+
+print_r($json_encoded); // {"a":1,"b":2,"c":3}
+print_r($json_decoded); // [a=>1, b=>2, c=>3]
