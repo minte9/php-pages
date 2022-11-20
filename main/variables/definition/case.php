489aec3d27f2b17602e300149df8852ded95c78a
minte9 <catalinprescure@gmail.com>
2022-11-20 21:35:07 +0200

    php-pages update

--- main/variables/definition/case.php
+++ main/variables/definition/case.php
@@ -1,13 +1,15 @@
 <?php
 /**
  * In PHP Variables are one of only two identifier types ...
- * that are case-sensitive. The other are constants.
+ * that are case-sensitive
+ * 
+ * The other are constants
  */
 
 $Name = "John";
-echo $name; 
-    // Undefined variable: name
-
 const NAME = "Bob";
-echo name;
+
+var_dump(isset($name)); // false
+
+//echo name;
     //Undefined constant: name
