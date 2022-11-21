e6dc22eeb027f2a4d7b7c4b977202d34af9931f5
minte9 <catalinprescure@gmail.com>
2022-11-21 17:21:29 +0200

    php-pages update

--- main/operators/comparison/inequality.php
+++ main/operators/comparison/inequality.php
@@ -2,20 +2,13 @@
 
 /** 
  * Comparing strings
+ *
+ * D > C
+ * a > A 
+ * 
+ * ASCII value of a (97)
+ * ASCII value of A (65)
  */
 
-$left = "ABC";
-$right = "ABD";
-
-echo (int) ($left > $right); // 0
-
-    // because the letter D in $right is higher ...
-    // than the corresponding letter C in $left
-
-$left = 'apple';
-$right = 'Apple';
-
-echo (int) ($left > $right); // 1
-
-    // because the ASCII value of the character a (97) is ...
-    // than that of the character A (65)
+var_dump("ABC" > "ABD"); // false
+var_dump("apple" > "Apple"); // true
