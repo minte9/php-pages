15a5b05c9cbc920be8042fa83f9889b7c49ba1bf
minte9 <catalinprescure@gmail.com>
2022-11-21 18:20:30 +0200

    php-pages update

--- main/function/definition/null.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// The concept of "void" functions does not really apply to PHP
-
-function name() {}
-
-echo (name() === NULL); // 1
--- main/function/definition/return.php
+++ main/function/definition/return.php
@@ -3,6 +3,8 @@
 /**
  * Return is not a function, it is a language construct
  * Parantheses are not required
+ * 
+ * Function returns NULL, if you don't return a value
  */
 
 function A() {
@@ -13,5 +15,10 @@ function B() {
     return 2; // faster
 }
     
-echo A(); // 1
-echo B(); // 2
+function C() {
+    // return null, by default
+}
+
+var_dump(A() == 1);    // true
+var_dump(B() == 2);    // true
+var_dump(C() == null); // true
