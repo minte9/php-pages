d5a16758ac4a778b8a20ebe979b2e28bc27410a1
minte9 <catalinprescure@gmail.com>
2022-11-21 17:36:45 +0200

    php-pages update

--- main/operators/comparison/identity.php
+++ main/operators/comparison/identity.php
@@ -8,4 +8,4 @@ $x1 = 'xn--google.com';
 $x2 = 'google.com';
 
 var_dump(stripos($x1, 'xn--') === 0); // true - Correct
-var_dump(stripos($x2, 'xn--') == 0); // true - Incorrect
+var_dump(stripos($x2, 'xn--') == 0);  // true - Incorrect
