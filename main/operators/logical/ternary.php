a321445e17589cc8027d4626e82d84fec83291fa
minte9 <catalinprescure@gmail.com>
2022-11-21 17:58:18 +0200

    php-pages update

--- main/operators/logical/ternary.php
+++ main/operators/logical/ternary.php
@@ -3,34 +3,23 @@
 /**
  * Ternary
  * 
- * x = expr1  ? expr2 : expr3
+ * x = expr1 ? expr2 : expr3
  * x = expr2 if expr1 = TRUE
- */
-
-$a = 1;
-$b = 1;
-
-echo ($a == $b  ? "true" : "false"); // true
-
-
-/**
+ * 
  * Ternary shorthand
  * 
  * x = expr1 ?: expr3
  * x = expr1 if expr1 = TRUE
- */
-
-$a = 1;
-$b = 2;
-
-echo ($a ?: $b); // 1
-
-
-/**
+ * 
  * Null coalescing
  * 
  * x = expr1 ?? expr2
  * x = expr1 if expr1 exists and not null
  */
 
- echo ($n ?? "None"); // None
+$a = 1;
+$b = 1;
+
+var_dump($a == $b ? "true" : "false"); // 'true'
+var_dump($a ?: $b); // 1
+var_dump($n ?? "None"); // 'None'
