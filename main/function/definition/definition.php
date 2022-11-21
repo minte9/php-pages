b36f15d21c2ba3cef7f6eff053bd38c9e2eff893
minte9 <catalinprescure@gmail.com>
2022-11-21 18:12:20 +0200

    php-pages update

--- main/function/definition/definition.php
+++ main/function/definition/definition.php
@@ -2,19 +2,10 @@
 
 /** 
  * Function names are case-insensitive
- * 
- * Having a function name() and adding a new one Name() ...
- * throws a Fatal error: Cannot redeclare Name()
  */
 
 function name() {
-    echo "1";
+    return 1;
 }
 
-if (! function_exists('name')) {
-    function Name() {
-        echo "2";
-    }
-}
-
-Name(); // "1"
+var_dump(Name() == 1); // true
