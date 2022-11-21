38bd80c77f9f993ca387dbb7c8284d7f182ded8d
minte9 <catalinprescure@gmail.com>
2022-11-21 17:08:07 +0200

    php-pages update

--- main/operators/comparison/identity.php
+++ main/operators/comparison/identity.php
@@ -4,8 +4,8 @@
  * Identical operator
  */
 
-$domain = 'xn--google.com';
-$domain2 = 'google.com';
+$x1 = 'xn--google.com';
+$x2 = 'google.com';
 
-var_dump(stripos($domain, 'xn--') === 0); // true - Correct
-var_dump(stripos($domain2, 'xn--') == 0); // true - Incorrect
+var_dump(stripos($x1, 'xn--') === 0); // true - Correct
+var_dump(stripos($x2, 'xn--') == 0); // true - Incorrect
