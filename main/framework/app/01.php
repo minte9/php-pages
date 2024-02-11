7c09c61786809a37fab5620b9f1c20209bcd0159
minte9 <catalinprescure@gmail.com>
2024-02-11 17:17:05 +0200

    php-pages update

--- main/framework/app/01.php
+++ main/framework/app/01.php
@@ -5,8 +5,10 @@
     cd github/php-pages/main/framework/app/
     php -S localhost:8000
 
-    http://localhost:8000/01.php?name=John  # Hello John
-    http://localhost:8000/01.php            # Warning: Undefined array key "name" 
+    http://localhost:8000/01.php?name=John
+        Hello John
+    http://localhost:8000/01.php
+        Warning: Undefined array key "name" 
 */
 
 ini_set("display_errors", 1);
