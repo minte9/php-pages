dc6106f798981060a3ae2d24824809643f67b9ac
minte9 <catalinprescure@gmail.com>
2025-02-17 09:52:17 +0200

    php-pages update

---
+++ main/oop/namespaces/no_collision.php
@@ -0,0 +1,31 @@
+<?php
+/**
+    To avoid class name collision we use namespaces
+ */
+
+namespace VendorA;
+
+ // Logger class from VendorA
+class Logger {
+    public function log($message) {
+        echo "VendorA Logger: " . $message;
+    }
+}
+
+namespace VendorB;
+
+// Logger class from VendorB
+class Logger {
+    public function log($message) {
+        echo "VendorB Logger: " . $message;
+    }
+}
+
+$logger = new Logger();
+$logger->log("Message from logger");
+
+/**
+    Output:
+
+    VendorB Logger: Message from logger
+ */
