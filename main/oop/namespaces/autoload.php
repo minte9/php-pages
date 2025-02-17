2c5b201e29d0cf4f98869d6662068acd20339aa0
minte9 <catalinprescure@gmail.com>
2025-02-17 11:31:05 +0200

    php-pages update

---
+++ main/oop/namespaces/autoload.php
@@ -0,0 +1,16 @@
+<?php
+/**
+    This autoloader automatically converts namespace paths into file paths 
+    and include them dynamically.
+ */
+
+function classAutoLoader($class) {
+    // Convert namespace to full file path
+    $file = __DIR__ . '/vendor/' . str_replace("\\", "/", $class . ".php");
+
+    if (file_exists($file)) {
+        require $file;
+    }
+}
+
+spl_autoload_register("classAutoLoader");
---
+++ main/oop/namespaces/autoload_way.php
@@ -0,0 +1,33 @@
+<?php
+/**
+    Using autoloading via PSR-4.
+    This is the standar way to load classes without requiring each file manually.
+
+    Project structure:
+    my_project/
+        vendor/
+            VendorA/
+                Logger.php
+            VendorB/
+                Logger.php
+    autoload.php
+    autoload_way.php
+ */
+
+require("autoload.php");
+
+use vendorA\Logger as LoggerA;
+use vendorB\Logger as LoggerB;
+
+$loggerA = new LoggerA();
+$loggerA->log("Logging with vendorA");
+
+$loggerB = new LoggerB();
+$loggerB->log("Logging with vendorB");
+
+/**
+    > php autoload_way.php
+
+    VendorA Logger: Logging with vendorA
+    VendorB Logger: Logging with vendorB
+ */
