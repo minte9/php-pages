c71b4dc1080a7334012bb4727988d0962ccf6a54
minte9 <catalinprescure@gmail.com>
2025-09-26 12:03:57 +0300

    php-pages update

--- main/basics/reference/functions.php
+++ main/basics/reference/functions.php
@@ -1,13 +1,15 @@
 <?php
 /**
  * By default, function arguments in PHP are passed by value (copied).
- * If you want a function to modify the value, you pass it by reference (not recommended).
+ * If you want a function to modify the value, you pass it by reference.
  */
 
-function addFive(&$x) {
-    $x += 5;
+function addUser(&$users, $user) {
+    $users[] = $user;
 }
 
-$a = 10;
-addFive($a);
-echo $a;   // 15
+$team = ["Alice", "Bob"];
+addUser($team, "John");
+addUser($team, "Mary");
+
+print_r($team);  // Alice, Bob, John, Mary
