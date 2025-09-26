52ef7bc4fefb74a82fc2cf6860e4cf0bd56187e7
minte9 <catalinprescure@gmail.com>
2025-09-26 12:37:57 +0300

    php-pages update

--- main/basics/reference/reference.php
+++ main/basics/reference/reference.php
@@ -4,7 +4,7 @@
  * A reference allows two different variable names to point to the same memory location.
  */
 
-$a =  10; 
-$b =& $a;  // $b references $a 
-$b =  20;
+$a = 10; 
+$b = &$a;  // $b references $a 
+$b = 20;
 echo $a;   // 20
