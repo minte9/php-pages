4256f337778f4736d5c9cfbbfd37d5927e3e05e7
minte9 <catalinprescure@gmail.com>
2025-09-29 14:04:03 +0300

    php-pages update

---
+++ main/function/anonymous/anonymous.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * An anonymous function is a function without a name.
+ * There are also called closures or lamba functions.
+ * 
+ * It is often used as callback function, especially you don't need to reuse the function.
+ * Be aware of the ; after function declaration.
+ */
+
+ $greet = function($name) {
+    return "Hello, $name!";
+};
+
+echo $greet('Alice');  // Hello, Alice!
---
+++ main/function/anonymous/import_variables.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Anonymous function can inherit variables from the parent scope,
+ * using the keyword 'use'.
+ */
+
+$message = "Welcome";
+
+$greet = function($name) use ($message) {
+    return "$message, $name!";
+};
+
+echo $greet("Alice");  // Welcome, Alice!
