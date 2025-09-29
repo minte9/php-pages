a79bfb94dd5c638d7a21cc61512fd53d095b3843
minte9 <catalinprescure@gmail.com>
2025-09-29 14:21:25 +0300

    php-pages update

--- main/function/anonymous/anonymous.php
+++ main/function/anonymous/anonymous.php
@@ -2,8 +2,6 @@
 /**
  * An anonymous function is a function without a name.
  * There are also called closures or lamba functions.
- * 
- * It is often used as callback function, especially you don't need to reuse the function.
  * Be aware of the ; after function declaration.
  */
 
