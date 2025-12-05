e1216c66b2bea319bfe6659be08883ae45bb55ae
minte9 <catalinprescure@gmail.com>
2025-12-05 17:47:38 +0200

    php-pages update

--- main/design-patterns/service_layer/index.php
+++ main/design-patterns/service_layer/index.php
@@ -4,8 +4,3 @@ require __DIR__ . '/vendor/autoload.php';
 
 $app = new App\App();
 $app->run();
-
-/*
-    EMAIL SENT / Subject: Welcome  / welcome_guide.pdf
-    EMAIL SENT / Subject: Reset Password  / instructions.txt
-*/
