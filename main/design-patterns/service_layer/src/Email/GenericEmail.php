f2dd58952e0c5761fec5833c986f82efdbe456a4
minte9 <catalinprescure@gmail.com>
2025-12-05 17:05:29 +0200

    php-pages update

--- main/design-patterns/service_layer/src/Email/GenericEmail.php
+++ main/design-patterns/service_layer/src/Email/GenericEmail.php
@@ -4,18 +4,12 @@ namespace App\Email;
 
 class GenericEmail
 {
-    public string $to;
-    public string $subject;
-    public string $body;
-    public array $attachments;
-
-    public function __construct(string $to, string $subject, string $body, array $attachments)
-    {
-        $this->to = $to;
-        $this->subject = $subject;
-        $this->body = $body;
-        $this->attachments = $attachments;
-    }
+    public function __construct(
+        public string $to, 
+        public string $subject, 
+        public string $body, 
+        public array $attachments
+    ) {}
 
     public function attach(string $filePath): self
     {
